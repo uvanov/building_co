f2fafb3a1ef8083aa60a39920c2754d373b246a7
Kirill I <54040921+uvanov@users.noreply.github.com>
2021-02-24 12:54:30 +0200

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,18 @@
 $(document).ready(() => {
+
+
     let upBtn = $('.up-button');
     $(window).on('scroll', () => {
-        if(parseInt($(window).scrollTop()) >= 1000){
+        if (parseInt($(window).scrollTop()) >= 1000) {
             upBtn.addClass('show');
-        }else{
+        } else {
             upBtn.removeClass('show');
         }
     })
 
-    upBtn.on('click', function(e) {
+    upBtn.on('click', function (e) {
         e.preventDefault();
-        $('html, body').animate({scrollTop:0}, '300');
+        $('html, body').animate({ scrollTop: 0 }, '300');
     });
 
 
@@ -34,7 +36,7 @@ $(document).ready(() => {
     let popupStatus = false;
     let popupOverlay = $('.popup-overlay');
 
-    $('.request__btn').each(function(i){
+    $('.request__btn').each(function (i) {
         $(this).on('click', () => {
             popupStatus = true;
             popupOverlay.fadeIn();
@@ -43,23 +45,85 @@ $(document).ready(() => {
         })
     })
 
-    $('.popup-close').on('click', function(){
+    $('.popup-close').on('click', function () {
         popupStatus = false;
         popupOverlay.fadeOut();
         $('.wrapper').css('filter', 'none');
         console.log('Close click');
     })
 
-    $(document).mouseup(function(e){
+    $(document).mouseup(function (e) {
         let popup = $('.popup');
-        if(e.target != popup[0] && popup.has(e.target).length === 0){
+        if (e.target != popup[0] && popup.has(e.target).length === 0) {
             popupOverlay.fadeOut();
             $('.wrapper').css('filter', 'none');
-        }else{
+        } else {
             console.log('etarger', e.target);
             console.log('has', e.target);
             console.log('etarger', e.target);
         }
+    })
+
+
+    // Menu
+
+    $('.burger-btn').on('click', () => {
+        $('.header__navbar--mobile').toggleClass('active');
+    });
+
+
+    $('.navbar__close').on('click', () => {
+        $('.header__navbar--mobile').toggleClass('active');
+    });
+
+
+    $('[class*=js-]').on('click', () => {
+        $('.header__navbar--mobile').removeClass('active');
+    });
+
+    $('.js-projects').on('click', () => {
+        $('html,body').animate({
+            scrollTop: $('.projects').offset().top - $('.projects').height() / 2
+        }, 1000);
+    });
+
+    $('.js-services').on('click', () => {
+        $('html,body').animate({
+            scrollTop: $('.services').offset().top - $('.services').height() / 2
+        }, 1000);
+    });
+
+    $('.js-faq').on('click', () => {
+        $('html,body').animate({
+            scrollTop: $('.question').offset().top - $('.question').height() / 2
+        }, 1000);
+    });
+
+    $('.js-partners').on('click', () => {
+        $('html,body').animate({
+            scrollTop: $('.partners').offset().top - $('.partners').height() / 2
+        }, 1000);
+    });
+
+    $('.js-reviews').on('click', () => {
+        $('html,body').animate({
+            scrollTop: $('.reviews').offset().top - $('.reviews').height() / 2
+        }, 1000);
+    });
+
+    $('.js-contacts').on('click', () => {
+        $('html,body').animate({
+            scrollTop: $('.contacts').offset().top - $('.contacts').height() / 2
+        }, 1000);
+    });
+
+    // Faq
+
+    $('.question__text').slideToggle();
+
+    $('.question__title').on('click', function(){
+        $(this).siblings('.question__text').slideToggle();
+        $(this).children('svg').toggleClass('rotated');
     })
     
 
